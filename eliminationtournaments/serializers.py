45b997db1c32d14b782192b4f3be979d4bfba219
marcelobbfonseca <marcelobbff@gmail.com>
2023-07-06 14:20:48 -0300

    feat: updates position serializer

--- eliminationtournaments/serializers.py
+++ eliminationtournaments/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Tournament, Player, Position
+from .models import Tournament, Player, Position, Match
 
 
 class PlayerSerializer(serializers.ModelSerializer):
@@ -8,10 +8,13 @@ class PlayerSerializer(serializers.ModelSerializer):
         fields = ('id', 'avatar', 'name')
 
 class PositionSerializer(serializers.ModelSerializer):
-    player = PlayerSerializer(read_only=True)
+    player = PlayerSerializer(read_only=True)  
+    next_position = serializers.PrimaryKeyRelatedField(read_only=True, required=False)
+    # next = PositionSerializer(read_only=True, required=False, source='next_position')
+    depth = 1
     class Meta:
         model = Position
-        fields = ('id', 'order', 'votes', 'player', 'next_position')
+        fields = ('id', 'order', 'votes', 'player', 'next_position', 'left_position', 'right_position')
 
 class TournamentSerializer(serializers.HyperlinkedModelSerializer):
     position_set = PositionSerializer(many=True, read_only=True)
@@ -22,3 +25,9 @@ class TournamentSerializer(serializers.HyperlinkedModelSerializer):
                   'current_round', 'total_rounds', 'match_time',
                   'position_set')
 
+class MatchSerializer(serializers.HyperlinkedModelSerializer):
+    position_one = PositionSerializer(read_only=True)
+    position_two = PositionSerializer(read_only=True)
+    class Meta:
+        model = Match
+        fields = ('position_one', 'position_two' 'disabled', 'voted', 'round')
